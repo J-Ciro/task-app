5be00601f6a68e706047c611abea263f3e9262fa
J-Ciro <jciro.1984@gmail.com>
2025-05-02 20:46:54 -0500

    feat(app): improved component architecture and componentization for home, categories and tasks

---
+++ src/app/home/components/task-list/task-list.component.ts
@@ -0,0 +1,49 @@
+// task-list.component.ts
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonCheckbox,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss'],
+  standalone: true,
+  imports: [
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonCheckbox,
+    IonIcon,
+    CommonModule,
+  ],
+})
+export class TaskListComponent {
+  @Input() tasks: any[] = [];
+  @Input() title: string = '';
+  @Input() icon: string = '';
+  @Input() emptyMessage: string = 'No tasks';
+  @Output() toggle = new EventEmitter<string>();
+  @Output() taskClick = new EventEmitter<string>();
+
+  onToggleTask(taskId: string) {
+    this.toggle.emit(taskId);
+  }
+
+  onTaskClick(taskId: string) {
+    this.taskClick.emit(taskId);
+  }
+}
